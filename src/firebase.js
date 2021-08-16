dbf5c96548ca6f594ab7a1a93f2d0219b74e2025
SG <sgprajapati24@gmail.com>
2021-08-16 16:25:30 +0530

    Header Complete

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyp8jLu7kgx3exj3Yq0Qw3KDrWtnH1VAo",
+  authDomain: "slack-clone-cad81.firebaseapp.com",
+  projectId: "slack-clone-cad81",
+  storageBucket: "slack-clone-cad81.appspot.com",
+  messagingSenderId: "301211958071",
+  appId: "1:301211958071:web:bdc547916de8f22016f8b2"
+};
